53c7c36404163512c7d1d229d1388242174a5e44
Daniel <danielbarros854@yahoo.com>
2021-08-30 18:40:03 -0300

    Att search

--- src/main/searchUser.js
+++ src/main/searchUser.js
@@ -1,8 +1,14 @@
 import { Users } from "../dataBase/models/userModel";
 
 export const searchUser = async (req,res) => {
-    const search = await Users.findAll({
-        where: {id: req.body.id}
-    });
-    res.send(search);
+    try {
+
+        const search = await Users.findOne({
+            where: {id: req.body.id}
+        });
+        res.send((search) ? search : "User not defined");
+        
+    } catch (error) {
+        res.send(error);
+    }
 }
