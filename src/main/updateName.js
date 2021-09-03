7a79393d5e979b5c462a1edcec5d95334920afb6
Daniel <danielbarros854@yahoo.com>
2021-09-03 11:51:24 -0300

    att in update

--- src/main/updateName.js
+++ src/main/updateName.js
@@ -2,21 +2,43 @@ import { Users } from "../dataBase/models/userModel";
 
 export const updateName = async (req, res) => {
     try {
-        const [numberOfAffectedRows , affectedRows] = await Users.update({
+        const {newName, newEmail, newPassword, id} = req.body;
+        const update = await Users.findByPk(id);
+        
+        //update name
+        if (newName != "" || newName == update.name) {
+            update.name = newName;
+        }
+        //update email
+        if (newEmail != "" || newEmail == update.email) {
+            update.email = newEmail;
+        }
+        //update password
+        if (newPassword != "" || newPassword == update.password) {
+            update.password = newPassword;
+        }
+        
+        update.save();
+        res.send(update);
+
+    } catch (error) {
+        res.send(`Error: ${error}`)
+    }
+    //[numberOfAffectedRows , affectedRows]
+    /*
+    const {newName, newEmail, newPassword, id} = req.body;
+    const user = await Users.update({
             name: (req.body.name)
         }, {
             where: {id: req.body.id},
-            returning: true,
-            plain: true
-        })
+            returning: true
+        }).then((user) => console.log(user))
+        
+    */
+   /*
         if (affectedRows != 0) {
             res.send("update complited")
         } else {
             res.send("User not found")
-        };
-        
-
-    } catch (error) {
-        res.send(error)
-    }
+        };*/
 }
